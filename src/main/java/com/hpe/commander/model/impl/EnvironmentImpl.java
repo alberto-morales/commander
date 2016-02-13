ff28a7fe8dc2fcf8001d8cebb6ced68e85069619
Alberto Morales <mail@albertomorales.eu>
2016-02-13 12:29:38 +0100

    Tratamiento del start-stop mediante threads, no iniciando uno hasta que el anterior no ha iniciado, y acumulando resultados tras terminar todas las threads. Inverse order del stop.

--- src/main/java/com/hpe/commander/model/impl/EnvironmentImpl.java
+++ src/main/java/com/hpe/commander/model/impl/EnvironmentImpl.java
@@ -1,5 +1,7 @@
 package com.hpe.commander.model.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import com.hpe.commander.model.Environment;
@@ -7,32 +9,65 @@ import com.hpe.commander.model.Server;
 
 public class EnvironmentImpl implements Environment {
 
-	public EnvironmentImpl(List<Server> serverList) {
-		super();
-		this.serverList = serverList;
-	}
+    public EnvironmentImpl(List<Server> paramServerList) {
+        super();
+        this.paramServerList = paramServerList;
+    }
 
-	@Override
-	public String start() {
+    @Override
+    public String start() {
+        String resultado = "Nothing done!!!";
+        resultado = startStop(paramServerList, Operation.START);
+        return resultado;
+    }
+
+    @Override
+    public String stop() {
+        String resultado = "Nothing done!!!";
+        ArrayList<Server> reverseOrderList = new ArrayList<Server>(paramServerList);
+        Collections.reverse(reverseOrderList);
+        resultado = startStop(reverseOrderList, Operation.STOP);
+        return resultado;
+    }
+
+	private String startStop(List<Server> serverList, Operation operation) {
 		String resultado = "";
-		for (Server server : serverList) {
-			resultado += "\n\rStarting "+server.getDescription()+"\n\r";
-			resultado += server.start();
-			resultado += "\n\r"+server.getDescription()+" started \n\r";
-		}
+		List<StartStopRunnable> startersList = new ArrayList<StartStopRunnable>();
+        for (Server server : serverList) {
+            StartStopRunnable starter = new StartStopRunnable(server, operation);
+            Thread thread = new Thread(starter);
+            thread.start();
+            while (!starter.isStarted()) {
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            };
+            startersList.add(starter);
+        }
+        for (StartStopRunnable starter : startersList) {
+            while (!starter.isFinished()) {
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+        for (StartStopRunnable starter : startersList) {
+            resultado += "\n\r";
+            resultado += operation.equals(Operation.START) ? "Starting" : "Stoppping";
+            resultado += " " + starter.getDescription()+"\n\r";
+            resultado += starter.getResult();
+            resultado += "\n\r" + starter.getDescription()+" ";
+            resultado += operation.equals(Operation.START) ? "started" : "stopped";
+            resultado += " \n\r";
+        }
 		return resultado;
 	}
 
-	@Override
-	public String stop() {
-		String resultado = "";
-		for (Server server : serverList) {
-			resultado += "\n\rStoping "+server.getDescription()+"\n\r";
-			resultado += server.stop();
-			resultado += "\n\r"+server.getDescription()+" stoped \n\r";
-		}
-		return resultado;
-	}
-
-	private List<Server> serverList = null;
+    private List<Server> paramServerList = null;
 }
